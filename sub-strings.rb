1c8da61602b94aac4cce3efe4948088fa2736e08
Magmusacy <xpfk.098@gmail.com>
2020-05-15 16:02:42 +0200

    Planned how to make the program work

---
+++ sub-strings.rb
@@ -0,0 +1,5 @@
+=begin
+create method substrings that takes two arguements, word (string) and an array (dictionary) respectively 
+check if each element of array is included in the string case insensitive
+return a hash with keys as a string that occur in the array and values saying how many times they occur
+=end
